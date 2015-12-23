ad286c550e1d74193cee596e59af35e5d818d4ec
Jacek Modrakowski <jacek.modrakowski@scalac.io>
2015-12-23 14:45:07 +0100

    Adds initial state of QR scanner. Borrowed from google vision samples.

--- app/src/main/java/io/scalac/degree/android/activity/RegisterUserActivity.java
+++ app/src/main/java/io/scalac/degree/android/activity/RegisterUserActivity.java
@@ -1,9 +1,13 @@
 package io.scalac.degree.android.activity;
 
+import android.content.Intent;
+
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
+import io.scalac.degree.utils.Logger;
 import io.scalac.degree33.R;
+import scanner.BarcodeCaptureActivity;
 
 /**
  * www.scalac.io
@@ -13,8 +17,22 @@ import io.scalac.degree33.R;
 @EActivity(R.layout.activity_register_user)
 public class RegisterUserActivity extends BaseActivity {
 
+    private static final int RC_BARCODE_CAPTURE = 1578;
+
     @Click(R.id.registerUserViaNfc)
     void onNfcClick() {
         NfcScanningActivity_.intent(this).start();
+    }
+
+    @Click(R.id.registerUserViaQr)
+    void onScannerClick() {
+        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Logger.l("onActivityResult: " + data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
