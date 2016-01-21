7ef11ad35b066e04a9343c44645ba12dca45d0e1
Jacek Modrakowski <jacek.modrakowski@scalac.io>
2016-01-21 19:47:13 +0100

    Adds filter with days.

---
+++ app/src/main/java/io/scalac/degree/data/conference/ConferenceManager.java
@@ -0,0 +1,52 @@
+package io.scalac.degree.data.conference;
+
+import org.androidannotations.annotations.EBean;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import io.scalac.degree.data.conference.model.ConferenceDay;
+
+@EBean
+public class ConferenceManager {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+
+    public void fetchAvailableConferences() {
+        // TODO TBD
+    }
+
+    public void fetchConferenceData(String confCode) {
+        // TODO TBD
+    }
+
+    public List<ConferenceDay> getConferenceDays() {
+        // TODO Test values.
+        final String fromDate = "2015-11-09T01:00:00.000Z";
+        final String toDate = "2015-11-13T23:00:00.000Z";
+        final DateTime fromConfDate = convertStringDate(fromDate);
+        final DateTime toConfDate = convertStringDate(toDate);
+
+        final int daysSpan = Days.daysBetween(
+                fromConfDate.toLocalDate(), toConfDate.toLocalDate()).getDays();
+
+        final List<ConferenceDay> result = new ArrayList<>(daysSpan);
+        for (int i = 0; i < daysSpan; i++) {
+            final DateTime tmpDate = fromConfDate.plusDays(i);
+            result.add(new ConferenceDay(tmpDate.getMillis(),
+                    tmpDate.dayOfWeek().getAsText(Locale.getDefault())));
+        }
+
+        return result;
+    }
+
+    private DateTime convertStringDate(String stringDate) {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(DATE_FORMAT);
+        return formatter.parseDateTime(stringDate);
+    }
+}
